1ccdf52968245fe40d42004e96cdbbc6feef8f28
Brookelynn Constant <Brookelynn.constant@gmail.com>
2020-08-21 17:08:50 -0500

    updated trips/index form to take user values in the form and redirect

--- demo/src/main/java/org/travelMVP/demo/controllers/TripController.java
+++ demo/src/main/java/org/travelMVP/demo/controllers/TripController.java
@@ -3,7 +3,9 @@ package org.travelMVP.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,21 +14,33 @@ import java.util.List;
 @RequestMapping("trips")
 public class TripController {
 
+    //TODO: consider using a hashmap? each trip, should have an associated list of itin items
+    private static List<String> trips = new ArrayList<>();
+
     @GetMapping
     public String displayAllTrips(Model model){
-        //TODO: consider using a hashmap? each trip, should have an associated list of itin items
-        List<String> trips = new ArrayList<>();
-        trips.add("Romania 2019");
-        trips.add("Switzerland 2019");
-        trips.add("Portugal 2020");
-        trips.add("Tennessee USA 2020");
+        //TODO: refactor this so it's not a static list
+//        List<String> trips = new ArrayList<>();
+//        trips.add("Romania 2019");
+//        trips.add("Switzerland 2019");
+//        trips.add("Portugal 2020");
+//        trips.add("Tennessee USA 2020");
+//        model.addAttribute("trips", trips);
         model.addAttribute("trips", trips);
         return "trips/index";
     }
 
-    //routes to trips/create
+    //get mapping handles routes to trips/create
     @GetMapping("create")
+    //TODO: refactor so it takes string name and a date
     public String renderCreateEventForm(){
         return "trips/create";
+    }
+
+    //post mapping handles routes to trips/create
+    @PostMapping("create")
+    public String createTrip(@RequestParam String tripName){
+        trips.add(tripName);
+        return "redirect:"; //redirect to root p ath for controller trips in this case
     }
 }
