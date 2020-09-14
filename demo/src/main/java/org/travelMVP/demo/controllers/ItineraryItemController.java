f4664809d605cc0e6a2286414b63cdef4879e61b
Brookelynn Constant <Brookelynn.constant@gmail.com>
2020-09-13 20:14:18 -0500

    updated itinerary controller

--- demo/src/main/java/org/travelMVP/demo/controllers/ItineraryItemController.java
+++ demo/src/main/java/org/travelMVP/demo/controllers/ItineraryItemController.java
@@ -1,9 +1,43 @@
 package org.travelMVP.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequestMapping(value="itineraries")
 public class ItineraryItemController {
 
     //TODO: figure out the itinerary item controller
+    List<String> itinerary = new ArrayList<>();
+
+    @GetMapping
+    public String displayItinItems(Model model){
+
+//        List<String> itinerary = new ArrayList<>();
+        itinerary.add("flight");
+        itinerary.add("hotel");
+        itinerary.add("train");
+        itinerary.add("brewery tour");
+        model.addAttribute("itinerary", itinerary);
+        return "/itineraries/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreateItineraryItemForm(){
+        return "itineraries/create";
+    }
+
+    //post mapping handles routes to itineraries/create
+    @PostMapping("create")
+    public String createItineraryItem(@RequestParam String itinItem){
+        itinerary.add(itinItem);
+        return "redirect:"; //redirect to root path for controller trips in this case
+    }
+
+
+
 }
